6a23804ba142ff7c8f9444caa249fdcd2612580c
Mizbaaa1 <misbachettali@gmail.com>
2026-07-30 13:05:00 +0300

    my first page created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="content">
+        <h1 className="title">
+          Smart Civic Issue Reporting System
+        </h1>
+
+        <p className="description">
+          Report civic issues easily and help make your community
+          better.
+        </p>
+
+        <div className="button-group">
+          <button className="btn report-btn">
+            Report an Issue
+          </button>
+
+          <button className="btn login-btn">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
